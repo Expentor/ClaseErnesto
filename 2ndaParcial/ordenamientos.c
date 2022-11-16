6c6aabd0418a90186617db7938fcd24a5ec625f8
Expentor <adairfernandez.aafs@gmail.com>
2022-11-16 11:37:21 -0600

    Busqueda Secuencial

--- 2ndaParcial/ordenamientos.c
+++ 2ndaParcial/ordenamientos.c
@@ -15,7 +15,7 @@ int particion(int *A, int izq, int der){
 		while(A[izq] < pivote){ //compara que todos
 			izq++; //los elementos a la derecha sean mayores al pivote
 		}
-		while(A[der] > pivote){ // compara todos los elementos de la derecha con el pivote
+		while(A[der] > pivote){ // compara todos los elementos de la izquierda con el pivote
 			der--;
 		}
 		if(izq >= der){
@@ -87,6 +87,15 @@ void leer_archivo(){
 	}while(c!= EOF);
 }
 
+void sort(int *A, int n, int key){
+	for(int i=0; i<=n; i++){
+		if (A[i] == key) {
+			printf("He encontrado el numero en la posicion: %d \n",A[i]);
+		}else{
+			printf("No existe ese numero dentro del array");
+		}
+	}
+}
 void examen(int *A, int n){
 	int saco;
 	for(int i=0; i<n; i++){
@@ -99,33 +108,102 @@ void examen(int *A, int n){
 		}
 	}
 }
- 
+
+void busqueda_secuencial(int *A, int n, int e){
+	int pos = 0;
+	while(pos<n){
+		if(A[pos] == e){
+			printf("El dato %d se encontro en la posicion: %d \n",e, pos);
+			return 0;
+		}else{
+			pos = pos+1;
+		}
+	}
+	printf("No se encontró el numero dentro del arreglo\n");
+}
+
 int main(){
 
 	int n = 5;
+	int opc = 9;
+	do {
+		printf("***Bienvenido al programa***\n");
+		printf("Seleccione el metodo de ordenamiento que deseas utilizar: \n");
+		printf("1. QuickSort\n");
+		printf("2. BubbleSort\n");
+		printf("3. InsertionSort\n");
+		printf("4. Código del examen\n");
+		printf("5. Búsqueda secuencial\n");
+		printf("9. Salir");
+		printf("\n");
+		scanf("%d",&opc);
+		printf("\n\n");
 
+		switch (opc)
+		{
+		case 1:
+			leer_archivo();
+			printf("Input: ");
+			pantalla(A, n);
+			quicksort(A, 0, n-1);
+			printf("Output: ");
+			pantalla(A, n);
+			printf("\n");
+			break;
+		case 2:
+			leer_archivo();
+			printf("Input: ");
+			pantalla(A, n);
+			bubble(A, n);
+			printf("Output: ");
+			pantalla(A, n);
+			printf("\n");
+			break;
+		case 3:
+			leer_archivo();
+			printf("Input: ");
+			pantalla(A, n);
+			insertion_sort(A, n);
+			printf("Output: ");
+			pantalla(A, n);
+			printf("\n");
+			break;
+		case 4:
+			leer_archivo();
+			printf("Input: ");
+			pantalla(A, n);
+			examen(A, n);
+			printf("Output: ");
+			pantalla(A, n);
+			printf("\n");
+			break;
+		case 5:
+			int e = 0;
+			leer_archivo();
+			printf("Input: ");
+			pantalla(A, n);
+			printf("¿Qué numero quieres buscar dentro del array? \n");
+			scanf("%d", &e);
+			busqueda_secuencial(A, n, e);
+			printf("\n");
+			break;
+		case 9:
+			return 0;
+			break;
+		default:
+			printf("Seleccione una opción válida la próxima vez \n");
+			return 0;
+		}
+	}while(opc!=0);
 	// printf("Escriba el tamaño del array: ");
 	// scanf("%d", &n);
-
 	// printf("Array de %d posiciones\n", n);
 
 	// for(int i = 0; i < n; i++){
 		// printf("Escriba el numero de la posicion %d: ", i+1);
 	// 	scanf("%d", &A[i]);
 	// }
-
-	leer_archivo();
-
-	printf("Input: ");
-	pantalla(A, n);
-
 	//insertion_sort(A, n);
 	//bubble(A, n);
 	//quicksort(A,0,n-1);
-	examen(A, n);
-
-	printf("Output: ");
-	pantalla(A, n);
-
-	printf("\n");
 }
