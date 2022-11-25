2b3a3c41f82e03882603fb2b06caf7032fb497aa
Expentor <adairfernandez.aafs@gmail.com>
2022-11-25 12:14:31 -0600

    Clase de 25/11/2022

--- 2ndaParcial/arboles.c
+++ 2ndaParcial/arboles.c
@@ -5,19 +5,41 @@ struct node{
     struct node* left_child;
     struct node* right_child;
 
-    int info;
-};//missing ""1
+    int data;
+};
 
 struct node* newNode(int data){
     size_t node_size = sizeof(struct node);
     struct node* new_Node = (struct node*) malloc(node_size);
-    new_Node -> info = data;
+    new_Node -> data = data;
     new_Node -> left_child = NULL;
     new_Node -> right_child = NULL;
 
     return new_Node;
 }
 
-int main(){
+void insertNode(struct node* nd, int data){
+    if(data>nd->data){
+        if(nd->right_child==NULL){
+            nd->right_child=newNode(data);
+        }else{
+            insertNode(nd->right_child,data);
+        }
+    }else{
+        if(nd->left_child==NULL){
+            nd->left_child=newNode(data);
+        }else{
+            insertNode(nd->left_child,data);
+        }
+    }
+}
 
+
+int main(){
+    struct node* root=NULL;
+    root=newNode(8);
+    insertNode(root,3);
+    insertNode(root,20);
+
+    return 0;
 }
